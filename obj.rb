d8d945d0206baafdf8f7b0475131fc47875c9201
BuildTools <unconfigured@null.spigotmc.org>
2016-09-03 05:22:46 +0800

    Object

--- obj.rb
+++ obj.rb
@@ -4,4 +4,15 @@ def obj.talk
 	puts "(Do you love me?)"
 end
 
+count = Object.new
+def count.count
+	number = 2 + 2
+	print number
+end
+	
+def obj.c2f(c)
+	c * 9.0 / 5 + 32
+end
+
 obj.talk
+puts obj.c2f(100)
