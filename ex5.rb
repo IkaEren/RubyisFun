877fe18ebebb8fd35f334857ea77ab34eaec8fd9
BuildTools <unconfigured@null.spigotmc.org>
2016-09-08 06:07:55 +0800

    Excerise q.q

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zamiel Chia'
+my_age = 17 # 1999 bby
+my_height = 169 / 30.48  # cm
+my_weight = 62 # kg fk
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "He's #{my_weight}kg heavy"
+puts "Actually that's not very tall"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right.
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}"
